526bf6e2f5fdcdb563abd138f4525a23814f91dc
Felipe <felipe@pop-os.localdomain>
2021-09-16 20:13:27 -0300

    FindAll increment

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,10 @@ public class Program {
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for (Seller obj : list) out.println(obj);
+
+        out.println("===TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
+        for (Seller obj : list) out.println(obj);
     }
 
 
